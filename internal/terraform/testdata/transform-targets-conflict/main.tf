d308f6d8185f4934601204b1b1f110bdfec4585b
Th3will <wni1717@gmail.com>
2021-11-13 16:29:41 -0800

    refined testing scenario in transform-targets-conflict

--- internal/terraform/testdata/transform-targets-conflict/main.tf
+++ internal/terraform/testdata/transform-targets-conflict/main.tf
@@ -1,22 +1,5 @@
 resource "aws_vpc" "me" {}
 
-resource "aws_subnet" "me" {
-  depends_on = [
-    aws_vpc.me,
-  ]
-}
-
-resource "aws_instance" "me" {
-  depends_on = [
-    aws_subnet.me,
-  ]
-}
-
-resource "aws_instance" "notmeeither" {
-  depends_on = [
-    aws_instance.me,
-  ]
-}
 module "child" {
   source = "./child"
 }
